324c98bb67fe8866541ab8bf222b5382d3e0b546
musab174444@gmail.com <1744440musab>
2022-10-07 11:18:41 +0200

    Add test_raster_grid.py

---
+++ test_raster_grid.py
@@ -0,0 +1,6 @@
+from raster_grid import test_cell_center, test_locate_cell, test_number_of_cells, test_cell_iterator
+
+test_number_of_cells()
+test_locate_cell()
+test_cell_center()
+test_cell_iterator()
